f77bfdc37bfd0f1b1e3aa85406bd4567b7bcfb14
Edinilson Vida <edinilson.vida@hotmail.com>
2024-03-20 16:42:43 -0300

    Atualização do arquivo index.js
    Foi adicionada uma quarta linha.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.log("Estamos aprendendo JavaScript!");
 console.log("Segunda linha...");
-console.log("Terceira linha...")
+console.log("Terceira linha...");
+console.log("Quarta linha...");
